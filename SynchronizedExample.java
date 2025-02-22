828e20584868d8f16afd4d236ec828c60ca9116f
belle <ehoi.loveyourself@gmail.com>
2025-02-23 00:43:16 +0900

    feat: 데이터 충돌 문제 발생

---
+++ SynchronizedExample.java
@@ -0,0 +1,30 @@
+public class SynchronizedExample {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount();
+
+        Thread t1 = new Thread(() -> account.withdraw("스레드1", 70));
+        Thread t2 = new Thread(() -> account.withdraw("스레드2", 70));
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class BankAccount {
+    private int balance = 100;
+
+    public void withdraw(String name, int amount) {
+        if (balance >= amount) {
+            System.out.println(name + " 출금 시도: " + amount);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            balance -= amount;
+            System.out.println(name + " 출금 완료! 남은 잔액: " + balance);
+        } else {
+            System.out.println(name + " 출금 실패! 잔액 부족.");
+        }
+    }
+}
